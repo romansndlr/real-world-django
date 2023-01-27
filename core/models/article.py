55b5da5a3c50e4d71c77a6fc659a6fec0330d4d6
Roman Sandler <romansndlr@gmail.com>
2023-01-27 22:09:38 +0200

    wip

--- core/models/article.py
+++ core/models/article.py
@@ -7,7 +7,7 @@ class Article(models.Model):
     title = models.CharField(max_length=30)
     description = models.CharField(max_length=100)
     body = models.TextField()
-    tags = models.ManyToManyField(Tag)
+    tags = models.ManyToManyField(Tag, blank=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
